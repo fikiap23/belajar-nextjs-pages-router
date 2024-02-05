be261d7a32989129570025beac84a74dacfecb98
fikiaprian23 <fikiaprian23@gmail.com>
2024-02-05 15:39:04 +0700

    add skeleton loading

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,16 +1,12 @@
+import ProductView from '@/views/Product'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 
-type productType = {
-  id: number
-  name: string
-  price: number
-  size: string
-}
 const ProductPage = () => {
   const { push } = useRouter()
   const [isLogin, setIsLogin] = useState(true)
   const [products, setProducts] = useState([])
+
   useEffect(() => {
     if (!isLogin) {
       push('/auth/login')
@@ -25,30 +21,9 @@ const ProductPage = () => {
     })
   }, [])
   return (
-    <div>
-      <h1 className="text-3xl font-bold ">ProductPage</h1>
-      {/* product table */}
-      <table>
-        <thead>
-          <tr>
-            <th>No</th>
-            <th>Name</th>
-            <th>Price</th>
-            <th>Size</th>
-          </tr>
-        </thead>
-        <tbody>
-          {products.map((product: productType, index) => (
-            <tr key={product.id}>
-              <td>{index + 1}</td>
-              <td>{product.name}</td>
-              <td>{product.price}</td>
-              <td>{product.size}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <>
+      <ProductView products={products} />
+    </>
   )
 }
 
