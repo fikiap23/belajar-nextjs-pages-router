a626478e71e10f31c3ce449638bf710b8da02381
fikiaprian23 <fikiaprian23@gmail.com>
2024-02-08 16:18:06 +0700

    add ssg

--- src/pages/shop/[[...slug]].tsx
+++ src/pages/shop/[[...slug]].tsx
@@ -6,7 +6,7 @@ const ShopPage = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">ShopPage</h1>
-      <p>Slug: {query.slug ? query.slug.join('-') : ''}</p>
+      <p>Slug: {query.slug}</p>
     </div>
   )
 }
