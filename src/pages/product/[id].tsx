ced55725f7e12bf5a507e454ecde67e913fa5699
fikiaprian23 <fikiaprian23@gmail.com>
2024-02-08 20:03:33 +0700

    add SSR detail product

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,17 +1,48 @@
 import { fetcher } from '@/lib/swr/fetcher'
+import { ProductType } from '@/types/product.type'
 import DetailProductView from '@/views/DetailProduct'
 import { useRouter } from 'next/router'
 import useSWR from 'swr'
 
-const DetailProductPage = () => {
-  const { query } = useRouter()
-  const { data, error, isLoading } = useSWR(`/api/product/${query.id}`, fetcher)
-  // console.log(data)
+// CSR
+// const DetailProductPage = () => {
+//   const { query } = useRouter()
+//   const { data, error, isLoading } = useSWR(`/api/product/${query.id}`, fetcher)
+//   // console.log(data)
+//   return (
+//     <div>
+//       <DetailProductView product={isLoading ? {} : data.data} />
+//     </div>
+//   )
+// }
+
+// export default DetailProductPage
+
+// SSR
+const DetailProductPage = ({ product }: { product: ProductType }) => {
   return (
     <div>
-      <DetailProductView product={isLoading ? {} : data.data} />
+      <DetailProductView product={product} />
     </div>
   )
 }
 
 export default DetailProductPage
+
+export async function getServerSideProps({
+  params,
+}: {
+  params: { id: string }
+}) {
+  // console.log(params.id)
+  // fetch data
+  const res = await fetch(`http://localhost:3000/api/product/${params.id}`)
+  const response = await res.json()
+  //   console.log(response)
+
+  return {
+    props: {
+      product: response.data,
+    },
+  }
+}
