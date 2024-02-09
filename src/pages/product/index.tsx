e3140ef8d98f697d8cfa6bf42185bb2cfe04b5d9
fikiaprian23 <fikiaprian23@gmail.com>
2024-02-09 10:59:20 +0700

    add middlaeware

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,29 +1,11 @@
 import { fetcher } from '@/lib/swr/fetcher'
 import ProductView from '@/views/Product'
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
+
 import useSWR from 'swr'
 
 const ProductPage = () => {
-  const { push } = useRouter()
-  const [isLogin, setIsLogin] = useState(true)
-  // const [products, setProducts] = useState([])
-
-  useEffect(() => {
-    if (!isLogin) {
-      push('/auth/login')
-    }
-  }, [isLogin, push])
-
   const { data, error, isLoading } = useSWR('/api/product', fetcher)
 
-  // useEffect(() => {
-  //   fetch('/api/product').then((res) => {
-  //     res.json().then((response) => {
-  //       setProducts(response.data)
-  //     })
-  //   })
-  // }, [])
   return (
     <>
       <ProductView products={isLoading ? [] : data.data} />
